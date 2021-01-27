1765fcae1569d2a0876b21342310696a43040234
hyeonker <root7dll@gmail.com>
2021-01-27 16:23:40 +0900

    호스팅 등록

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['54.180.207.245']
+ALLOWED_HOSTS = ['54.180.207.245', 'hyeonker.cf']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
